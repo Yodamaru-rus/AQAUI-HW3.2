601a45c5b21df145e069897e29b42ebc51bb0927
Vitkovskiy <Vikovskiy1912@mail.ru>
2026-03-27 12:17:45 +0300

    create ex 3. Create screenshot in test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,12 +7,14 @@ test('login', async ({ page }) => {
     await page.goto("https://netology.ru/");
     await page.getByRole('link', { name: 'Войти' }).click();
     await page.getByText('Войти по почте').click();
+    await page.screenshot({ path: 'screenshot0.png' });
     await page.getByRole('textbox', { name: 'Email' }).click();
     await page.getByRole('textbox', { name: 'Email' }).fill(email);
     await page.getByRole('textbox', { name: 'Пароль' }).click();
     await page.getByRole('textbox', { name: 'Пароль' }).fill(password);
     await page.getByTestId('login-submit-btn').click();
-    await expect(page.locator('[data-testid="advanced-iframe"]')).toBeVisible();
+    await expect(page.locator('[data-testid="advanced-iframe"]').contentFrame().getByTestId('silhouette-container')).toBeVisible();
+    await page.screenshot({ path: 'screenshot1.png' });
     //await expect(page).toHaveURL('https://netology.ru/profile/9861028');
 });  
 
@@ -27,14 +29,5 @@ test('login falling', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Пароль' }).fill("7890");
     await page.getByTestId('login-submit-btn').click();
     await expect(page.getByText('Неверный email')).toBeVisible();
+    await page.screenshot({ path: 'screenshot2.png' });
 });  
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
